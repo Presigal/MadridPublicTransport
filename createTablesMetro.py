f8833af30282a8b1db8f23f7fae56fb37657a779
Presigal <alexcespedes20@gmail.com>
2025-01-24 19:47:22 +0100

    started creating the script to make all the tables, still pending

--- createTablesMetro.py
+++ createTablesMetro.py
@@ -1,24 +1,27 @@
 import MetroDeMadrid_db #file that contains the DB connection
 import json #importing JSON to read in MetroMadridTables.JSON
+import time
 
 #reading in the JSON
-with open('tables.json', 'r') as file:
-    table_definitions = json.load(file)
+
+with open('data/MetroDeMadrid/gtfs/MetroMadridTables.json', 'r') as file:
+    MetroTables = json.load(file)
+
+#This for loop creates a Dictionary, for the columns inside the tables have associated data types
+#for table_name, table_details in MetroTables.items():
+#    metroDataTypes = table_details["columns"]
+
+#print(metroDataTypes)
 
 try:
-    #connecting to the database. Imported the function for security
-    connection = MetroDeMadrid_db.get_connection()
-    #initializing the cursor
-    cursor = connection.cursor()
+    connection = MetroDeMadrid_db.get_connection()  #connecting to the database. Imported the function for security
+    cursor = connection.cursor()    #initializing the cursor
+    print("connection established!")
 
-    #The below query is to fetch all the information from the table #stops
-    query = "" #command to create all the tables
+    # Construct the CREATE TABLE SQL statement
+    columns_definitions = ", ".join([f"{col} {dtype}" for col, dtype in MetroTables.items()])
+    create_table_sql = f"CREATE TABLE {table_name} ({columns_definitions})"
 
-    # Execute the query
-    cursor.execute(query)
-    
-    rows = cursor.fetchall() # Fetch all rows
-    columns = [desc[0] for desc in cursor.description]  #extract the column names, as the above query doesnt return column names
 
     cursor.close()
     connection.close()
