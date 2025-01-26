0550dd5407bcc3e3c1c96ccdbe8738f4ac12404a
Presigal <alexcespedes20@gmail.com>
2025-01-26 10:35:20 +0100

    continued troubleshooting createTablesMetro.py

--- createTablesMetro.py
+++ createTablesMetro.py
@@ -7,7 +7,7 @@ import oracledb
 
 connection = MetroDeMadrid_db.get_connection()  #connecting to the database. Imported the function for security
 cursor = connection.cursor()    #initializing the cursor
-print("connection established!")
+print("connection established!\n")
 
 #reading in the JSON
 with open('data/MetroDeMadrid/gtfs/MetroMadridTables.json', 'r') as file:
@@ -17,25 +17,27 @@ with open('data/MetroDeMadrid/gtfs/MetroMadridTables.json', 'r') as file:
 #When inside of a "table", it will take the dictionary items and create the tables accordingly.
 for table_name, table_details in MetroTables.items():
     metroColumns = table_details["columns"]
-    
-    print(metroColumns)
-    time.sleep(1)
 
     #printing to see if its seeing it correctly
-    print(f"Creating table {table_name}")
+    print(f"Creating table {table_name}\n")
 
     # These two statements will be used to write the string for the Oracle command
     columns_definitions = ", ".join([f"{col} {dtype}" for col, dtype in metroColumns.items()])
-    create_table_sql = f"CREATE TABLE {table_name} ({columns_definitions})"
+    print("*******columns_definitions**********")
+    print(f"{columns_definitions}\n")
+    create_table_sql = f"CREATE TABLE {table_name} ({columns_definitions});"
+
+    print("**********create_table_sql*************")
+    print(f"{create_table_sql}\n")
 
     try:
         #this is the command itself. We are going to pass the string as an Oracle Command
         cursor.execute(create_table_sql)
-        print(f"Table {table_name} created successfully.")
+        print(f"Table {table_name} created successfully.\n")
 
     #error handling in case it doesnt work
     except oracledb.DatabaseError as e:
-        print(f"Error creating table {table_name}: {e}")
+        print(f"Error creating table {table_name}: {e}\n")
 
 cursor.close()
 connection.close()
